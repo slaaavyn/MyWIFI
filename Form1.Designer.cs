d97a5a132a2e99be4cf25ff86f684b0a9114db9c
Viacheslav Zhulov <slaaavyn@gmail.com>
2018-09-28 16:36:30 +0300

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MyWIFI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ssid = new System.Windows.Forms.MaskedTextBox();
+            this.pass = new System.Windows.Forms.MaskedTextBox();
+            this.passValid = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SSID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PASS:";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(12, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start WIFI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 128);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 164);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(132, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop WIFI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ssid
+            // 
+            this.ssid.Location = new System.Drawing.Point(53, 10);
+            this.ssid.Name = "ssid";
+            this.ssid.Size = new System.Drawing.Size(169, 20);
+            this.ssid.TabIndex = 7;
+            this.ssid.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ssid_KeyPress);
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(53, 53);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(169, 20);
+            this.pass.TabIndex = 8;
+            this.pass.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.pass_MaskInputRejected);
+            // 
+            // passValid
+            // 
+            this.passValid.AutoSize = true;
+            this.passValid.Location = new System.Drawing.Point(82, 76);
+            this.passValid.Name = "passValid";
+            this.passValid.Size = new System.Drawing.Size(37, 15);
+            this.passValid.TabIndex = 9;
+            this.passValid.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(82, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "V1.0 Developed by slaaavyn";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 314);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.passValid);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.ssid);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MyWIFI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MaskedTextBox ssid;
+        private System.Windows.Forms.MaskedTextBox pass;
+        private System.Windows.Forms.Label passValid;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace MyWIFI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            passValid.Text = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pass.TextLength < 9)
+            {
+                passValid.ForeColor = Color.Red;
+                passValid.Text = "short pass (min 9 sumbols)";
+            }
+            else
+            {
+                passValid.Text = null;
+
+                string commands = @"netsh wlan set hostednetwork mode=allow ssid=" + ssid.Text + " key=" + pass.Text +
+                    "\n netsh wlan start hostednetwork";
+
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        RedirectStandardInput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        RedirectStandardOutput = true,
+                        StandardOutputEncoding = Encoding.GetEncoding(866)
+                    }
+                };
+                process.Start();
+
+                using (StreamWriter pWriter = process.StandardInput)
+                {
+                    if (pWriter.BaseStream.CanWrite)
+                    {
+                        foreach (var line in commands.Split('\n'))
+                            pWriter.WriteLine(line);
+                    }
+                }
+
+                StreamReader read = process.StandardOutput;
+                textBox1.Text = read.ReadToEnd();
+                process.WaitForExit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string commands = @"netsh wlan stop hostednetwork";
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    StandardOutputEncoding = Encoding.GetEncoding(866)
+                }
+            };
+            process.Start();
+
+            using (StreamWriter pWriter = process.StandardInput)
+            {
+                if (pWriter.BaseStream.CanWrite)
+                {
+                    foreach (var line in commands.Split('\n'))
+                        pWriter.WriteLine(line);
+                }
+            }
+
+            StreamReader read = process.StandardOutput;
+            textBox1.Text = read.ReadToEnd();
+            process.WaitForExit();
+        }
+
+        private void pass_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+        }
+
+        private void ssid_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
